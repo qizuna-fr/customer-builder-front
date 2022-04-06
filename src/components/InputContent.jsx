92018f65bb094c1a8755afa25421759507c64b42
meriam <meriam.mahjoub@gmail.com>
2022-04-06 09:43:27 +0200

    inputComponent jest testing

--- src/components/InputContent.jsx
+++ src/components/InputContent.jsx
@@ -1,4 +1,5 @@
 import { setInputContent } from "../pages/utilities"
+import React from 'react'
 
 // import "../assets/css/Style.css"
 
@@ -11,7 +12,7 @@ export const InputContent = ({textInput}) => {
     return (
     <div>
         <h4>Commençons par le nom de votre {textInput} !</h4>
-        <p><input id="text" type = "text" placeholder="Tapez ici votre texte..." onInput={(e) => textContent(e.target.value)}></input></p>
+        <p><input aria-label="Some Label"  data-testid="text-input" type = "text" placeholder="Tapez ici votre texte..." onInput={(e) => textContent(e.target.value)}></input></p>
     </div> 
     )
 }
--- src/components/__tests__/InputContent.test.jsx
+++ src/components/__tests__/InputContent.test.jsx
@@ -1,10 +1,27 @@
-import { render } from '@testing-library/react'
+/**
+ * @jest-environment jsdom
+*/  
+
+import { render , screen, fireEvent  } from '@testing-library/react'
 import { InputContent } from "../InputContent" 
 import React from 'react'
+import '@testing-library/jest-dom'
 
-  test('test text input', () => {
-      render(<InputContent >  </InputContent>)
-      const inputEl = screen.getByTestId("text");
-      expect(inputEl).toBeInTheDocument();
-      expect(inputEl).toHaveAttribute("type", "text");
-  });
+test('The input field should be in the document', () => {
+  render(<InputContent />)
+  const inputEl = screen.getByTestId("text-input");
+  expect(inputEl).toBeInTheDocument();
+})
+
+test('The input field should have a type text attribute', () => {
+  render(<InputContent />)
+  const inputEl = screen.getByTestId("text-input");
+  expect(inputEl).toHaveAttribute("type", "text");
+})
+      
+test('The input field displayed the correct value', () => {
+  render(<InputContent />)
+  const input = screen.getByTestId("text-input");
+  fireEvent.change(input, { target: { value: 'Mulhouse' } })
+  expect(input.value).toBe('Mulhouse')
+});
